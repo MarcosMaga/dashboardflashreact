aa41323cd6515d1ffe62a6e180d1b1930987ab1d
Marcos Magalhães <53220617+MarcosMaga@users.noreply.github.com>
2022-06-08 22:11:55 -0300

    Add files via upload

--- src/components/QuemSomos/QuemSomos.js
+++ src/components/QuemSomos/QuemSomos.js
@@ -5,12 +5,9 @@ import TextAndTitle from "../TextAndTitle/TextAndTitle";
 function QuemSomos(){
     return(
         <div className="QuemSomos">
-            <br/>
-            <br/>
-            <br/>
-            <br/>
             <div className="panel">
                 <TextAndTitle title="Quem somos?" text="Olá, meu nome é Marcos Magalhães, sou aluno do Instituto Federal de São Paulo Campus Campinas. O objetivo deste site foi criar um Dashboard para a aula de Desenvolvimento Web do Professor André Bordignon."/>
+                <img alt="Imagem Instituto Federal" src="https://moodle.cmp.ifsp.edu.br/pluginfile.php/1/theme_academi/logo/1644248932/LogoIFSPCMP.jpg"/>     
             </div>
         </div>
     );
